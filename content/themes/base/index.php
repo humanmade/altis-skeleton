cde62758dcdfd1feb508797d0a687b37f5ecbfa2
tomjn <contact@tomjn.com>
2019-10-03 12:34:46 +0100

    add json decode safeguards and a closing paragraph tag

--- content/themes/base/index.php
+++ content/themes/base/index.php
@@ -12,13 +12,15 @@ $folder_path = 'content/themes/base';
 if ( file_exists( '/etc/chassis-constants' ) ) {
 	$json_string = file_get_contents( '/etc/chassis-constants' );
 	$data = json_decode( $json_string, true );
-	$folder_path = $data['synced_folders']['/chassis'].'/content/themes/base';
+	if ( !empty( $data ) && !empty( $data['synced_folders']['/chassis'] ) ) {
+		$folder_path = $data['synced_folders']['/chassis'].'/content/themes/base';
+	}
 }
 
 ?>
 	<div id="welcome">
 		<?php Altis\CMS\Branding\render_logo( 'white' ) ?>
-		<p><?php _e( 'Altis is installed and ready to go.', 'altis' ) ?>
+		<p><?php _e( 'Altis is installed and ready to go.', 'altis' ) ?></p>
 		<p><?php
 			echo sprintf(
 				__( 'Edit this placeholder theme at:<br><code>%s</code>', 'hm-platform' ),
